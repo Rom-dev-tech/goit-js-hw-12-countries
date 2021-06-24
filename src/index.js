13c87b69d30db88c02055f9f8b970762556d3b18
Nikolay Mosalov <Mosalov85@ukr.net>
2021-06-24 16:08:18 +0300

    adds markup

--- src/index.js
+++ src/index.js
@@ -1,40 +1,48 @@
-import './sass/main.scss';
-import getRefs from './js/getRefs';
+// import './sass/main.scss';
+// import getRefs from './js/getRefs';
+// import Countries from './js/fetchCountries';
 
-const debounce = require('lodash.debounce');
+// const debounce = require('lodash.debounce');
 
-// countries
-import countries from './js/countries'
+// // countries
+// import countries from './js/countries'
 
-// refs
-const refs = getRefs();
+// // refs
+// const refs = getRefs();
 
-// lisiner
-refs.input.addEventListener('input', debounce(onFilterChange, 500));
-
-
-function createListItemsMarkup(items) {
-  return items.map(item => `<li>${item}</li>`).join('');
-}
-
-function onFilterChange(evt) {
-  const filter = evt.target.value.toLowerCase();
+// // lisiner
+// refs.input.addEventListener('input', debounce(onFilterChange, 500));
 
 
-  const filteredItems = countries.filter(t =>
-    t.toLowerCase().includes(filter),
+// function createListItemsMarkup(items) {
+//   return items.map(item => `<li>${item}</li>`).join('');
+// }
+
+// function onFilterChange(evt) {
+//   const searchQuery = evt.target.value.toLowerCase();
+
+
+//   const filteredItems = countries.filter(t =>
+//     t.toLowerCase().includes(searchQuery),
+//   );
+
+//   const listItemsMarkup = createListItemsMarkup(filteredItems);
+//   populateList(listItemsMarkup);
+
+
+//   if (searchQuery === '') {
+//     refs.list.innerHTML = '';
+//   }
+// }
+
+// function populateList(markup) {
+//   refs.list.innerHTML = markup;
+// }
+
+// ================
+
+fetch('https://restcountries.eu/rest/v2/name/ukr')
+  .then(response => (response.json())
+    .then(console.log())
+    ,
   );
-
-  const listItemsMarkup = createListItemsMarkup(filteredItems);
-  populateList(listItemsMarkup);
-
-
-  if (filter === '') {
-    refs.list.innerHTML = '';
-  }
-}
-
-function populateList(markup) {
-  refs.list.innerHTML = markup;
-}
-
