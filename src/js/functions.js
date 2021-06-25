08b92563d874ff7eafe4ab1784f414a85a1dc608
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-06-25 16:06:28 +0300

    adds notifications

--- src/js/functions.js
+++ src/js/functions.js
@@ -2,13 +2,21 @@ import countryCardTpl from '../templates/country-card.hbs';
 import countriesList from '../templates/countries-list.hbs';
 import getRefs from '../js/getRefs';
 
+// PNotify
 import { alert, defaultModules } from '../../node_modules/@pnotify/core/dist/PNotify.js';
 import * as PNotifyMobile from '../../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
-
+import '../../node_modules/@pnotify/core/dist/BrightTheme.css';
+import { info } from '../../node_modules/@pnotify/core';
+import '../../node_modules/@pnotify/core/dist/PNotify.css';
+import '../../node_modules/@pnotify/core/dist/BrightTheme.css';
+import * as Confirm from '../../node_modules/@pnotify/confirm';
+import '../../node_modules/@pnotify/confirm/dist/PNotifyConfirm.css';
 defaultModules.set(PNotifyMobile, {});
 
+// refs
 const refs = getRefs();
 
+// functions
 function createsCountriesMarkup(counries) {
   const arrayLenght = counries.length;
   const OneCountryMarkup = counries.map(countryCardTpl).join('');
@@ -16,20 +24,43 @@ function createsCountriesMarkup(counries) {
 
   if (arrayLenght === 1) {
     renderMarkup(OneCountryMarkup);
+
+    info({
+      title: 'Congratulations ',
+      text: 'The requested country was found',
+      modules: new Map([
+        [
+          Confirm,
+          {
+            confirm: true,
+            buttons: [
+              {
+                text: 'Ok',
+                primary: true,
+                click: notice => {
+                  notice.close();
+                },
+              },
+            ],
+          },
+        ],
+      ]),
+    });
   }
 
   if (arrayLenght > 2 && arrayLenght < 10) {
     renderMarkup(countryListMarkup);
 
     alert({
-      text: 'Много стран под коитерием',
+      text: 'Please enter information more specifically',
     });
   }
 
   if (arrayLenght > 10) {
     renderMarkup('');
+
     alert({
-      text: 'Веддите название более коректней',
+      text: 'Many countries have been found. Please be more specific',
     });
   }
 }
@@ -37,7 +68,7 @@ function createsCountriesMarkup(counries) {
 function onFetchError(error) {
   console.log(error);
   alert({
-    text: 'Введите название правильно',
+    text: 'Country not found. Try again..',
   });
 }
 
